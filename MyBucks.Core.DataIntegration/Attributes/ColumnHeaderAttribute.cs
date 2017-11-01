fa0904141ea2cbabc152c055e29e340481526359
Tjaart Blignaut <tjaartblig@gmail.com>
2017-11-01 10:07:27 +0200

    Column header attribute

---
+++ MyBucks.Core.DataIntegration/Attributes/ColumnHeaderAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBucks.Core.DataIntegration.Attributes
+{
+    public class ColumnHeaderAttribute : System.Attribute
+    {
+        public ColumnHeaderAttribute(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
