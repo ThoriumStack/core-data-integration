0dd94aad2359ddbaca48592dcf3a42e9b17182c9
Tjaart Blignaut <tjaart@getbucks.com>
2017-11-15 15:28:29 +0200

    add an action for files with only a single type of data

--- MyBucks.Core.DataIntegration/InputBuilder.cs
+++ MyBucks.Core.DataIntegration/InputBuilder.cs
@@ -28,21 +28,54 @@ namespace MyBucks.Core.DataIntegration
             return this;
         }
 
+        /// <summary>
+        /// Add an error action if an error occurs on read
+        /// </summary>
+        /// <param name="errorAction"></param>
+        /// <returns></returns>
         public IInputBuilder WithErrorAction(Action<Exception> errorAction)
         {
             _errorAction = errorAction;
             return this;
         }
 
+        /// <summary>
+        /// Read one of a record type. i.e. headers and footers
+        /// </summary>
+        /// <typeparam name="TData"></typeparam>
+        /// <typeparam name="TDiscriminator"></typeparam>
+        /// <param name="assignAction"></param>
+        /// <param name="discriminator"></param>
+        /// <returns></returns>
         public IInputBuilder ReadOnce<TData, TDiscriminator>(Action<TData> assignAction, Func<TDiscriminator, bool> discriminator) where TDiscriminator : new() where TData : new()
         {
             _buildActions.Add(() => _serializer.ReadSingle(assignAction, discriminator, _stream));
             return this;
         }
 
+        /// <summary>
+        /// Read multiple records by a discriminating factor
+        /// </summary>
+        /// <typeparam name="TData"></typeparam>
+        /// <typeparam name="TDiscriminator"></typeparam>
+        /// <param name="destination"></param>
+        /// <param name="discriminator"></param>
+        /// <returns></returns>
         public IInputBuilder ReadMany<TData, TDiscriminator>(IList<TData> destination, Func<TDiscriminator, bool> discriminator) where TData : new() where TDiscriminator : new()
         {
             _buildActions.Add(() => _serializer.ReadMany(destination, discriminator, _stream));
+            return this;
+        }
+
+        /// <summary>
+        /// Read a data stream with only one type of record
+        /// </summary>
+        /// <typeparam name="TData"></typeparam>
+        /// <param name="destination"></param>
+        /// <returns></returns>
+        public IInputBuilder ReadAll<TData>(IList<TData> destination) where TData : new()
+        {
+            _buildActions.Add(() => _serializer.GetData<TData>(_stream));
             return this;
         }
 
