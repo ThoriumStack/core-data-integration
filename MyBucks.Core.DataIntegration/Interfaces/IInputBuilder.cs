ecbb888ab6388df871adccb0f1d5d314cd19dd04
Tjaart Blignaut <tjaart@getbucks.com>
2017-11-15 15:37:41 +0200

    oops

--- MyBucks.Core.DataIntegration/Interfaces/IInputBuilder.cs
+++ MyBucks.Core.DataIntegration/Interfaces/IInputBuilder.cs
@@ -11,7 +11,7 @@ namespace MyBucks.Core.DataIntegration.Interfaces
         IInputBuilder SetData(MemoryStream stream);
         IInputBuilder ReadOnce<TData, TDiscriminator>(Action<TData> assignAction, Func<TDiscriminator, bool> discriminator) where TData : new() where TDiscriminator : new();
         IInputBuilder ReadMany<TData, TDiscriminator>(IList<TData> destination, Func<TDiscriminator, bool> discriminator) where TData : new() where TDiscriminator : new();
-        IInputBuilder ReadAll<TData>(IList<TData> destination);
+        IInputBuilder ReadAll<TData>(IList<TData> destination) where TData : new();
         IInputBuilder WithErrorAction(Action<Exception> errorAction);
         void Build();
     }
