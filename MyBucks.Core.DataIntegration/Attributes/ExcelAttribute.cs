2373ef02518176ae6680156d12b81977a921ec22
Donald Asante <Donaldasante@mybucks.com>
2018-09-11 14:45:54 +0200

    Added Excel Attribute

---
+++ MyBucks.Core.DataIntegration/Attributes/ExcelAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBucks.Core.DataIntegration.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Class)]
+    public class ExcelAttribute : System.Attribute
+    {
+        public bool ShowHeader { get; } = true;
+
+        public String Column { get; } = "A";
+        public int ColumnNumber { get; }
+
+        public int Row { get; } = 1;
+
+        public ExcelAttribute(String column, int row, Boolean showHeader = true)
+        {
+            Column = column;
+            Row = row;
+            ShowHeader = showHeader;
+            ColumnNumber = ExcelAttribute.ExcelColumnNameToNumber(column);
+        }
+
+        private static int ExcelColumnNameToNumber(String columnName)
+        {
+            if (string.IsNullOrEmpty(columnName)) throw new ArgumentNullException("columnName");
+
+            columnName = columnName.ToUpperInvariant();
+
+            int sum = 0;
+
+            for (int i = 0; i < columnName.Length; i++)
+            {
+                sum *= 26;
+                sum += (columnName[i] - 'A' + 1);
+            }
+
+            return sum;
+        }
+    }
+}
